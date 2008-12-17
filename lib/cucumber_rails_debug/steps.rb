ef3144a1274c63a3b302f337d023441e86f88eaf
Mischa <f.mischa@gmail.com>
2008-12-17 03:32:33 -0800

    readme and steps'

--- lib/cucumber_rails_debug/steps.rb
+++ lib/cucumber_rails_debug/steps.rb
@@ -3,10 +3,16 @@ Then /^debug$/ do
   debugger
 end
 
-# Then /^html$/ do
-#   puts @world.reponse.body
-# end
-# 
-# Then /^params$/ do
-#  puts @world.response.request.parameters
-# end
+Then /^html$/ do
+  display { puts @response.body.gsub("\n", "\n \e    ")}
+end
+
+ Then /^params$/ do
+  display{ puts @request.parameters.inspect }
+ end
+
+def display(decoration="\n#{'*' * 80}\n")
+  puts decoration
+  yield
+  puts decoration 
+end
